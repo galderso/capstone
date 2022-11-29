69774b9e52b550226d5815352d9d4979338cb2f7
Kenzie218 <mvolleyballjohnson@gmail.com>
2022-11-29 17:34:50 -0500

    added Jump to cpp

--- checkers.cpp
+++ checkers.cpp
@@ -10,7 +10,7 @@ bool Checkers::Kinged(vector<vector<char>> board,int row1,int col1){
     }
     return true;
 }
-bool Checkers::Move(vector<vector<char>> board,int row1,int col1,int row2, int col2){
+bool Checkers::Move(vector<vector<char>> &board,int row1,int col1,int row2, int col2){
     if(Kinged(board,row1,col1)==false){// if not kinged
         if(board[row1][col1]=='x'){
             if((row2==row1+1&&row2==row1+2)&&(col2==col1+1&&col2==col1-1&&col2==col1+2&&col2==col1-2)){
@@ -62,6 +62,10 @@ void Checkers::Display(vector<vector<char>> board){
     }
 }
 
+//Remove the opponent's piece when jumping over it
+void Checkers::Jump(vector<vector<char>> &board, int row1, int col1, int row2, int col2){
+  
+}
 
 
 /*
