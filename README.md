89a2f90e509463246b9f3cfe5b748b8a456bb28a
galderso <gmalderson4321@yahoo.com>
2022-11-29 16:01:30 -0500

    added board

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+
+Git commands:
 "git push" -pushes current local system to branch
 "git pull"  -pulls updated files from github repository to local system
-"git checkout -b [name]" -makes a new branch
+"git checkout -b "name" -makes a new branch
 "git add ." -adds all local system files to branch
-"git commit -m [message]" -puts commit message to know what was changed/updated
+"git commit -m "message" -puts commit message to know what was changed/updated
--- checkers.cpp
+++ checkers.cpp
@@ -1,1 +1,74 @@
 //checkers
+#include<iostream>
+#include<vector>
+
+using namespace std;
+bool winner(vector<vector<int>> board){
+    int count1=0,count2=0;
+    for(int i =0 ;i<8;i++){
+        for(int j = 0;j<8;j++){
+            if((board[i][j]=='x')&&(board[i][j]=='X')){
+                count1++;
+            }
+            else if((board[i][j]=='o')&&(board[i][j]=='O')){
+                count2++;
+            }
+        }
+    }
+    if(count1==0||count2==0){
+        return true;
+    }
+    return false;
+}
+int main(){
+    cout <<"Welcome to checkers"<<endl;
+
+   vector<vector<char>> board{
+         {' ','x',' ','x',' ','x',' ','x'},
+         {'x',' ','x',' ','x',' ','x',' '},
+         {' ',' ',' ',' ',' ',' ',' ',' '},
+         {' ',' ',' ',' ',' ',' ',' ',' '},
+         {' ',' ',' ',' ',' ',' ',' ',' '},
+         {' ',' ',' ',' ',' ',' ',' ',' '},
+         {' ','o',' ','o',' ','o',' ','o'},
+         {'o',' ','o',' ','o',' ','o',' '}
+         };
+         while((winner(board))!=false){
+            do{
+         cout<<"  0 1 2 3 4 5 6 7"<<endl;
+         for(int i =0;i<8;i++){
+            cout<< i<<'|';
+          
+            for(int j = 0;j<8;j++){
+                cout<<board[i][j]<<'|';
+            }
+            cout<<endl;
+         }
+    
+    cout<< "Enter the piece you want to move"<<endl;
+    int row1,col1,row2,col2;
+    cin>>row1>>col1;
+    cout<< "Enter the place you want to move to"<<endl;
+    cin>>row2>>col2;
+
+    if(move()){
+        swap(board[row1][col1],board[row2][col2]);
+    }
+    }while(jumpAgain());   
+}
+}  
+
+/*
+
+   void swap(int i,int j){
+    int temp = i;
+    i=j;
+    j=temp;
+   }
+*/
+    //swap :swaps element when player moves
+    //move:checks if a move is valid
+    //jump: checks if move is a jump and if jump is possible(inbetween is opposite)and removes inbetween
+    //jumpAgain: checks if jump again is possible
+    //winner: checks if x or o is no longer on the board
+
