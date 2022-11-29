ad49da7c1c428c793ec6bfdf2dd605a7866bcb0c
NicholasWandinata <nicholaswandinata@gmail.com>
2022-11-29 17:36:45 -0500

    stuff

--- main.cpp
+++ main.cpp
@@ -42,17 +42,12 @@ int main(){
             cout << "Enter the place you want to move to" << endl;
             cin >> row2 >> col2;
 
-            if(c.move() == true){
-                swap(board[row1][col1],board[row2][col2]);
+            if(c.move() == true && ((turn % 2 == 1 && (board[row1][col1] == 'o' || board[row1][col1] == 'O')) || (turn % 2 == 2 && (board[row1][col1] == 'x' || board[row1][col1] == 'X')))){
+                swap(board[row1][col1],board[row2][col2])
             }
             turn += 1;
-        }while(c.JumpPiece(Player1, Player2));   //FIX THIS!!!
+        } while(c.jump(Player1, Player2));   //FIX THIS!!!
     }
-<<<<<<< HEAD
-            }while(c.jump(Player1, Player2));   //FIX THIS!!!
-        }
-=======
 
     return 0;
->>>>>>> 53d83c8ae4b234c7809240da6cac1433cf16ccbe
 }
