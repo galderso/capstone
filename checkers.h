3dac2199448467ea2f7a0661f473cbd7936bafb2
Kenzie218 <mvolleyballjohnson@gmail.com>
2022-11-29 17:54:27 -0500

    nun

--- checkers.h
+++ checkers.h
@@ -8,6 +8,5 @@ class Checkers{
         bool Move(vector<vector<char>> &board, int row1, int col1, int row2, int col2);
         void Jump(vector<vector<char>> &board, int row1, int col1, int row2, int col2);
         bool Kinged(vector<vector<char>> board,int row1,int col1);
-        bool Winner(vector<vector<char>> board);
         void Display(vector<vector<char>> board);
 };
