2d2a865cb6c5914b08c75ef29aa60f847086dd1a
Kenzie218 <mvolleyballjohnson@gmail.com>
2022-12-01 17:38:26 -0500

    added to jump

--- checkers.cpp
+++ checkers.cpp
@@ -1,10 +1,11 @@
 //checkers
 #include "checkers.h"
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <cctype>
 using namespace std;
 
-bool Checkers::Kinged(vector<vector<char>> board,int row1,int col1){//checks ifa piece is kinged or not
+bool Checkers::Kinged(vector<vector<char>> board,int row1,int col1){//checks if a piece is kinged or not
     if(board[row1][col1]!='X'&&board[row1][col1]!='O'){
         return false;
     }
@@ -15,27 +16,48 @@ bool Checkers::Jump(vector<vector<char>> &board, int row1, int col1, int row2, i
     //Could check to make sure the piece removed is not the current player's piece
     //Up and right
     if(row1 > row2 && col1 < col2){
-        board[row1 - 1][col1 + 1] = ' ';
+        if(toupper(board[row1 - 1][col1 + 1]) != toupper(board[row1][col1])){ //accomadates for pieces that could be kings
+            board[row1 - 1][col1 + 1] = ' ';
+            return true;
+        }
+        else{
+            return false;
+        }
         //check if can jump again
         //if not, then return true
     }
     //Up and left
     else if(row1 > row2 && col1 > col2){
-        board[row1 - 1][col1 - 1] = ' ';
-        return;
+        if(toupper(board[row1 - 1][col1 - 1]) != toupper(board[row1][col1])){
+            board[row1 - 1][col1 - 1] = ' ';
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     //Make sure the piece is a king
     if(Kinged(board, row1, col1)){
         //Down and right
         if(row1 < row2 && col1 > col2){
-            board[row1 + 1][col1 - 1] = ' ';
-            return;
+            if(toupper(board[row1 + 1][col1 - 1]) != toupper(board[row1][col1])){
+                board[row1 + 1][col1 - 1] = ' ';
+                return true;
+            }
+            else{
+                return false;
+            }
         }
         //Down and left
         else if(row1 < row2 && col1 < col2){
-            board[row1  + 1][col1 + 1] = ' ';
-            return;
+            if(toupper(board[row1 + 1][col1 + 1]) != toupper(board[row1][col1])){
+                board[row1 + 1][col1 + 1] = ' ';
+                return true;
+            }
+            else{
+                return false;
+            }
         }
     }
 }
