81d370047aa334bb990ee0bf760e37aeda6c6c66
NicholasWandinata <nicholaswandinata@gmail.com>
2022-12-01 16:08:00 -0500

    stuff

--- checkers.h
+++ checkers.h
@@ -3,10 +3,15 @@
 using namespace std;
 
 class Checkers{
+    private:
+        char Player1;
+        char Player2;
+
     public:
         bool Winner(vector<vector<char>> board);
-        bool Move(vector<vector<char>> &board, int row1, int col1, int row2, int col2);
-        void Jump(vector<vector<char>> &board, int row1, int col1, int row2, int col2);
+        bool Move(vector<vector<char>> board,int row1,int col1,int row2, int col2);
+        void Jump(char piece1, char piece2);
         bool Kinged(vector<vector<char>> board,int row1,int col1);
+        bool Winner(vector<vector<char>> board);
         void Display(vector<vector<char>> board);
 };
